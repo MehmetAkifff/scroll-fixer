97d489f549f960b5cbc945f13e1055ff06cb7cf6
MehmetAkifff <makifergn@gmail.com>
2026-07-02 16:33:44 +0300

    Initial Commit

---
+++ mouseSwitcher/mouseSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouseSwitcherApp.swift
+//  mouseSwitcher
+//
+//  Created by Mehmet Akif ERGANİ on 2.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct mouseSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
